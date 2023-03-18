90f039397df8c4bec946f145dffb1c1da994acc5
mehmetkino <mehmetkino@yahoo.com>
2023-03-18 12:00:16 -0400

    hi this is my daily stand up routine

---
+++ new_modle/src/new_feature.java
@@ -0,0 +1,7 @@
+public class new_feature {
+    public static void main(String[] args) {
+        System.out.println("hi i got tis project from vsc , it was in github there people teams working on i clone it and got it " +
+                "");
+    }
+
+}
